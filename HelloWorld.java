2f6a0c020477ee606de507b5a8812c5a63176fba
Saket Titurkar <sakettiturkar005@gmail.com>
2024-07-17 23:51:42 +0530

    Fixed the defect #324 for HelloWorld syntax

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+	public static void main(String[] args) { 
+		var number = 10; // inferred as int
+		var c = 'a'; // inferred as char
+		System.out.println("i ;" + number);
+		System.out.println("i ;" + c);
+	
+    }
+    
+}
