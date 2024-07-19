067a683fd6cb3bf1e973f66d03eb276c12db92a1
Saket Titurkar <sakettiturkar005@gmail.com>
2024-07-19 23:03:09 +0530

    Modified ForTested file for renaming vairable p,q,r

--- learning/ForTested.java
+++ learning/ForTested.java
@@ -4,6 +4,12 @@ public class ForLoopTest {
      //This file is changed but will be commited directly without       staging.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+        int p,q,r;
+        p = 10;
+        q = 11;
+        r = 5;
+        
         int i;
         //for (initiazation;condition;increment)
         for(i=0;i < 3;) {
